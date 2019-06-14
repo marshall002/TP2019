a295e78aa5eb95a90c00196479972a9423092ce2
Christian Bacilio de la cruz <dbacilio88@gmail.com>
2019-06-13 22:11:45 -0500

    Monitorear Cita
    lista

--- DAO/ConexionBD.cs
+++ DAO/ConexionBD.cs
@@ -13,11 +13,16 @@ namespace DAO
         {
             get
             {
+<<<<<<< Updated upstream
 
                 return @"data source=LACING202A-09; initial catalog=BD_SCLAP; integrated security=SSPI;";
 
                 //return @"data source=LACING202A-06; initial catalog=BD_SCLAP; integrated security=SSPI;";
 
+=======
+                //return @"data source=DESKTOP-UH5SKAR\LENOVOPC; initial catalog=BD_SCLAP; integrated security=SSPI;";
+                return @"data source=LACING202A-03; initial catalog=BD_SCLAP; integrated security=SSPI;";
+>>>>>>> Stashed changes
 
                 //return @"data source=LAPTOP-TG82GILV; initial catalog=BD_SCLAP; integrated security=SSPI;";
                 //return @"data Source=DEVELOPER;Initial Catalog=DB_SCLAP;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;";
